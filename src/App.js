a81fb9f6e119b2a6ae60d7eb18314aa316a79521
ChrisGloom1 <89384429+ChrisGloom1@users.noreply.github.com>
2022-12-13 23:10:18 +0100

    created nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/routes/home/home.component";
+import Navigation from "./components/routes/navigation/navigation.component";
+
+
+const Shop = () => {
+  return(
+    <h1>I AM SHOP</h1>
+  )
+}
 
 const App = () => {
 
   return (
-    <>
-      <Directory />
-    </>
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home/>} />
+        <Route path="/shop" element={<Shop/>} />
+      </Route>
+    </Routes>
   );
 };
 
