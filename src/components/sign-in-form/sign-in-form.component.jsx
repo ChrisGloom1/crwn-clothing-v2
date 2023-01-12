8e298c0f680b148983499f781ab89fd09de9a438
ChrisGloom1 <89384429+ChrisGloom1@users.noreply.github.com>
2023-01-12 20:25:16 +0100

    created context for user

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import FormInput from "../form-input/form-input.component"
 import "./sign-in-form.styles.scss"
 import Button from "../button/button.component"
+import { UserContext } from "../../contexts/user.context"
 import { 
   signInWithGooglePopup, 
   createUserDocumentFromAuth,
@@ -18,6 +19,8 @@ const SignInForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields)
   const {email, password} = formFields
 
+  const { setCurrentUser } = useContext(UserContext)
+
   const resetFormFields = () => {
     setFormFields(defaultFormFields)
   }
@@ -30,8 +33,8 @@ const SignInForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault()
     try {
-      const r = await signInAuthUserWithEmailAndPassword(email, password)
-      console.log(r)
+      const {user} = await signInAuthUserWithEmailAndPassword(email, password)
+      setCurrentUser(user)
       resetFormFields()
     } catch(error) {
       switch(error.code){
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import FormInput from "../form-input/form-input.component"
 import "./sign-up-form.styles.scss"
+import { UserContext } from "../../contexts/user.context"
 import Button from "../button/button.component"
 import { 
   createAuthUserWithEmailAndPassword, 
@@ -19,6 +20,8 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields)
   const {displayName, email, password, confirmPassword} = formFields
 
+  const { setCurrentUser } = useContext(UserContext)
+
   const resetFormFields = () => {
     setFormFields(defaultFormFields)
   }
@@ -31,6 +34,7 @@ const SignUpForm = () => {
     }
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password)
+      setCurrentUser(user)
       await createUserDocumentFromAuth(user, { displayName })
       resetFormFields()
     } catch(error) {
