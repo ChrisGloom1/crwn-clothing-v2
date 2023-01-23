d70d0e3cade27a2d62cb4a1cd62f17ca05288164
ChrisGloom1 <89384429+ChrisGloom1@users.noreply.github.com>
2023-01-23 16:27:14 +0100

    created/finished setup for context and observer

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,7 +1,6 @@
-import { useState, useContext } from "react"
+import { useState } from "react"
 import FormInput from "../form-input/form-input.component"
 import "./sign-up-form.styles.scss"
-import { UserContext } from "../../contexts/user.context"
 import Button from "../button/button.component"
 import { 
   createAuthUserWithEmailAndPassword, 
@@ -20,8 +19,6 @@ const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields)
   const {displayName, email, password, confirmPassword} = formFields
 
-  const { setCurrentUser } = useContext(UserContext)
-
   const resetFormFields = () => {
     setFormFields(defaultFormFields)
   }
@@ -34,7 +31,6 @@ const SignUpForm = () => {
     }
     try {
       const { user } = await createAuthUserWithEmailAndPassword(email, password)
-      setCurrentUser(user)
       await createUserDocumentFromAuth(user, { displayName })
       resetFormFields()
     } catch(error) {
